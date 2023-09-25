7608e01d1d80bdf6d17953a5385633afc8d9da41
Teju-Anil <127083204+Teju-Anil@users.noreply.github.com>
2023-09-25 10:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+#container {
+    margin: 20px;
+    position: relative; /* Set position to relative for pseudo-elements to work */
+}
+
+#container::before {
+    content: "Before Text";
+    color: #f00;
+    font-size: 20px;
+    position: absolute;
+    top: 0;
+    left: -100px; /* Adjust the position as needed */
+}
+
+#container::after {
+    content: "After Text";
+    color: #f00;
+    font-size: 20px;
+    position: absolute;
+    bottom: 0;
+    right: -100px; /* Adjust the position as needed */
+}
